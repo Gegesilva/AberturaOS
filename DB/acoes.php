96b67fa784f5266b4b6558b5e2bf65053453fca7
Gegesilva <103137242+Gegesilva@users.noreply.github.com>
2025-05-13 15:19:16 -0300

    13-05

--- DB/acoes.php
+++ DB/acoes.php
@@ -17,131 +17,78 @@ function gravaOS($conn, $estado, $local, $email, $contpb, $serie, $whatsapp, $so
 {
     global $statusInicial, $novaOS;
 
-    /* Trata o numero de caracteres que será inserido no campo TB02115_NOME */
+    // Limites de tamanho
     $motivo = substr($defeito, 0, 50);
-
-    /* Trata o numero de caracteres que será inserido no campo TB02115_CELULAR */
-    $whatsapp = substr($whatsapp, 0, 11);
-
-    /* Trata o numero de caracteres que será inserido no campo TB02115_LOCAL */
+    $whatsapp = substr(preg_replace('/\D/', '', $whatsapp), 0, 11);
     $local = substr($local, 0, 200);
-
-    /* Trata o numero de caracteres que será inserido no campo TB02115_EMAIL */
     $email = substr($email, 0, 200);
-
-    /* Trata o numero de caracteres que será inserido no campo TB02115_SOLICITANTE */
     $solicitante = substr($solicitante, 0, 30);
-
-    /* Trata o numero de caracteres que será inserido no campo TB02115_SOLICITANTE */
     $contpb = substr($contpb, 0, 10);
 
-    /* Verifica se e patrimonio ou serie antes de gravar */
-    $sql = "SELECT TOP 1 
-                TB02112_NUMSERIE NumSerie
-            FROM TB02112
-            WHERE TB02112_PAT = '$serie'
-            AND TB02112_SITUACAO = 'A'
-    ";
-    $NumSerie = "";
-    $stmt = sqlsrv_query($conn, $sql);
-    while ($row = sqlsrv_fetch_array($stmt, SQLSRV_FETCH_ASSOC)) {
-        $NumSerie = $row['NumSerie'];
+    // Consulta o número de série real
+    $sqlSerie = "SELECT TOP 1 TB02112_NUMSERIE FROM TB02112 WHERE TB02112_PAT = ? AND TB02112_SITUACAO = 'A'";
+    $stmtSerie = sqlsrv_prepare($conn, $sqlSerie, [$serie]);
+    sqlsrv_execute($stmtSerie);
+    $rowSerie = sqlsrv_fetch_array($stmtSerie, SQLSRV_FETCH_ASSOC);
+    if ($rowSerie && isset($rowSerie['TB02112_NUMSERIE'])) {
+        $serie = $rowSerie['TB02112_NUMSERIE'];
     }
 
-    if ($NumSerie != NULL || $NumSerie != '') {
-        $serie = $NumSerie;
-    } else {
+    // Prepara o SQL de inserção com parâmetros
+    $sql = "
+    INSERT INTO TB02115(
+        TB02115_CODIGO, TB02115_DTCAD, TB02115_ESTADO, TB02115_LOCAL, TB02115_EMAIL, TB02115_CONTPB,
+        TB02115_NUMSERIE, TB02115_CELULAR, TB02115_SOLICITANTE, TB02115_OBS, TB02115_STATUS, TB02115_NOME,
+        TB02115_OPCAD, TB02115_CONTRATO, TB02115_CODEMP, TB02115_CODCLI, TB02115_TIPOINTERV, TB02115_PRODUTO,
+        TB02115_CODTEC, TB02115_ATENDENTE, TB02115_PREVENTIVA, TB02115_DATA, TB02115_SITUACAO,
+        TB02115_CEP, TB02115_END, TB02115_CIDADE, TB02115_BAIRRO, TB02115_NUM, TB02115_COMP
+    )
+    SELECT TOP 1 ?, GETDATE(), ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, 'APP ABERTURA_OS',
+           TB02112_CODIGO, TB02111_CODEMP, TB02111_CODCLI, 'I', TB02112_PRODUTO,
+           '0000', 'PortalQR', 'N', GETDATE(), 'A',
+           TB02112_CEP, TB02112_END, TB02112_CIDADE, TB02112_BAIRRO, TB02112_NUM, CAST(TB02112_COMP AS VARCHAR(20))
+    FROM TB02112
+    LEFT JOIN TB02111 ON TB02111_CODIGO = TB02112_CODIGO
+    WHERE TB02112_NUMSERIE = ? AND TB02112_SITUACAO = 'A'";
 
+    $params = [
+        $novaOS,
+        $estado,
+        $local,
+        $email,
+        $contpb,
+        $serie,
+        $whatsapp,
+        $solicitante,
+        "$defeito - Periodo para atendimento: $periodo",
+        $statusInicial,
+        $motivo,
+        $serie
+    ];
+
+    $stmt = sqlsrv_prepare($conn, $sql, $params);
+    if (!sqlsrv_execute($stmt)) {
+        print("Erro OS não gravada!!!");
     }
-
-    $sql = "INSERT INTO TB02115( 
-                TB02115_CODIGO,
-                TB02115_DTCAD,
-                TB02115_ESTADO,
-                TB02115_LOCAL,
-                TB02115_EMAIL,
-                TB02115_CONTPB,
-                TB02115_NUMSERIE,
-                TB02115_CELULAR,
-                TB02115_SOLICITANTE,
-                TB02115_OBS,
-                TB02115_STATUS,
-                TB02115_NOME,
-                TB02115_OPCAD,
-                TB02115_CONTRATO,
-                TB02115_CODEMP,
-                TB02115_CODCLI,
-                TB02115_TIPOINTERV,
-                TB02115_PRODUTO,
-                TB02115_CODTEC,
-                TB02115_ATENDENTE,
-                TB02115_PREVENTIVA,
-                TB02115_DATA,
-                TB02115_SITUACAO,
-                TB02115_CEP,
-                TB02115_END,
-                TB02115_CIDADE,
-                TB02115_BAIRRO,
-                TB02115_NUM,
-                TB02115_COMP)
-                (
-            SELECT TOP 1
-                '$novaOS',
-                GETDATE(),
-                '$estado',
-                '$local', 
-                '$email', 
-                '$contpb', 
-                '$serie', 
-                '$whatsapp',
-                '$solicitante',
-                '$defeito - Periodo para atendimento: $periodo',
-                '$statusInicial',
-                '$motivo',
-                'APP ABERTURA_OS',
-                TB02112_CODIGO,
-                TB02111_CODEMP,
-                TB02111_CODCLI,
-                'I',
-                TB02112_PRODUTO,
-                '0000',
-                'PortalQR',
-                'N',
-                GETDATE(),
-                'A',
-                TB02112_CEP,
-                TB02112_END,
-                TB02112_CIDADE,
-                TB02112_BAIRRO,
-                TB02112_NUM,
-                CAST(TB02112_COMP AS VARCHAR(20))
-            FROM TB02112
-            LEFT JOIN TB02111 ON TB02111_CODIGO = TB02112_CODIGO
-            WHERE TB02112_NUMSERIE = '$serie'
-            AND TB02112_SITUACAO = 'A')
-        ";
-
-    $stmt = sqlsrv_query($conn, $sql);
-    if ($stmt === false) {
-        //die(print_r(sqlsrv_errors(), true));
-        print ('Erro OS não gravada!!!');
-    }
-
 }
+
 
 function gravaHistorico($conn, $numOS, $serie, $defeito, $statusInicial)
 {
     /* Verifica se e patrimonio ou serie antes de gravar */
-    $sql = "SELECT TOP 1 
+    $sqlSerie = "SELECT TOP 1 
                 TB02112_NUMSERIE NumSerie
             FROM TB02112
             WHERE TB02112_PAT = '$serie'
             AND TB02112_SITUACAO = 'A'
     ";
-    $stmt = sqlsrv_query($conn, $sql);
-    while ($row = sqlsrv_fetch_array($stmt, SQLSRV_FETCH_ASSOC)) {
-        $NumSerie = $row['NumSerie'];
+    $stmtSerie = sqlsrv_prepare($conn, $sqlSerie, [$serie]);
+    sqlsrv_execute($stmtSerie);
+    $rowSerie = sqlsrv_fetch_array($stmtSerie, SQLSRV_FETCH_ASSOC);
+    if($rowSerie && isset($rowSerie['TB02112_NUMSERIE'])) {
+        $NumSerie = $rowSerie['NumSerie'];
     }
+    
 
     if (isset($NumSerie)) {
         $serie = $NumSerie;
@@ -150,7 +97,7 @@ function gravaHistorico($conn, $numOS, $serie, $defeito, $statusInicial)
     }
 
     $sql = "UPDATE TB00002
-            SET TB00002_COD = '$numOS'
+            SET TB00002_COD = ?
             WHERE TB00002_TABELA = 'TB02115'
     
             INSERT INTO TB02130
@@ -170,12 +117,12 @@ function gravaHistorico($conn, $numOS, $serie, $defeito, $statusInicial)
                 TB02130_HORASCOM,
                 TB02130_HORASFIM)
             SELECT TOP 1
-                '$numOS',
+                ?,
                 GETDATE(),
                 'APP ABERTURA_OS', 
-                '$statusInicial', 
+                ?, 
                 TB01073_NOME, 
-                '$defeito',
+                ?,
                 TB02115_CODTEC,
                 NULL,
                 TB01024_NOME, 
@@ -188,10 +135,19 @@ function gravaHistorico($conn, $numOS, $serie, $defeito, $statusInicial)
             FROM TB02115
             LEFT JOIN TB01073 ON TB01073_CODIGO = TB02115_STATUS
             LEFT JOIN TB01024 ON TB01024_CODIGO = TB02115_CODTEC
-            WHERE TB02115_NUMSERIE = '$serie'
+            WHERE TB02115_NUMSERIE = ?
             ORDER BY TB02115_DTCAD DESC";
 
-    $stmt = sqlsrv_query($conn, $sql);
+            $params = [
+                $numOS,
+                $numOS,
+                $statusInicial,
+                $defeito,
+                $serie
+            ];
+
+    $stmtSerie = sqlsrv_prepare($conn, $sql, $params);
+
 }
 
 
